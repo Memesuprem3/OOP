4b7cb141161de98cf958d8115e1df616a38cfa15
Christian Rapp <christian.rapp@edu.varberg.se>
2023-10-21 11:17:41 +0200

    Add project files.

---
+++ OOP/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Circle
+    {
+
+        public  double _pi = 3.141f;
+        public int _radius;
+
+        public Circle (int radius)
+        {
+            _radius = radius;
+
+        }
+
+
+
+        public double getArea()
+        {
+            return _pi * _radius * _radius;
+        }
+
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Circle circle1 = new Circle(6);
+            Circle circle2 = new Circle(5);
+
+            Console.WriteLine("Arean är: " + circle1.getArea() + " i cirkel 1");
+            Console.WriteLine("Arean är: " + circle2.getArea() + " i cirkel 2");
+
+
+        }
+    }
+}
